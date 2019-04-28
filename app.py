43f8778af448198655f0579cc1eba1e39f617ff8
ghigbie <gehigbie@gmail.com>
2019-04-28 09:32:02 -0600

    Add basic routes

--- app.py
+++ app.py
@@ -6,4 +6,24 @@ app = Flask(__name__)
 def home():
     return "Yo mochi!"
 
+@app.route('/store', methods=['POST'])
+def create_store():
+    pass
+
+@app.route('/store/<string:name>')
+def get_store(name):
+    pass
+
+@app.route('/store')
+def get_store():
+    pass
+
+@app.route('/store/<string:name>/item', methods=['POST'])
+def create_store_item():
+    pass
+
+@app.route('/store/<string:name>/item')
+def get_items_in_store(name):
+    pass
+
 app.run(port=5000)
